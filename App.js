212327d58a4fc03e94dc85f6473a9903d092fd60
OdedMagger <OdedMagger@gmail.com>
2020-07-22 15:24:46 +0300

    Testing git push from source tree

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
         <Text>ReNa-Eat</Text>
       </View>
       <View style={[styles.section, { height: 80, backgroundColor: 'grey' }]}>
-        <Text>Filters placeholder</Text>
+        <Text>Filters placeholder - just testing</Text>
       </View>
       <View style={styles.multiitemSection}>
         <RestaurantsList/>
